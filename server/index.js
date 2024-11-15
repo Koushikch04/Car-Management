b42334b2fbc0cbb6e6f995867340557ad74c5f76
KOUSHIK CHALLAGUNDLA <chk240404@gmail.com>
2024-11-15 14:40:02 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -95,7 +95,7 @@ app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 connectDB();
 
